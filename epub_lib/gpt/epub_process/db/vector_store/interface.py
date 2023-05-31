78049c4fcb96d5aec878ba987e04180a3ff0372d
cesarllb <cesar98.linares@gmail.com>
2023-05-31 15:52:45 -0400

    vector_store changes

--- epub_lib/gpt/epub_process/db/vector_store/interface.py
+++ epub_lib/gpt/epub_process/db/vector_store/interface.py
@@ -7,22 +7,23 @@ class IVectorStore(ABC):
     def  __init__(self, colection_name:str):
         ...
 
-    @abstractmethod
-    def  get_vector(self, word):
-        ...
 
     @abstractmethod
     def add(document:list, metadata:dict, ids:list):
         ...
 
-    @abstractmethod
-    def create_collection(self, colection_name:str):
-        ...
+    # @abstractmethod
+    # def get_vector(self, word):
+    #     ...
+
+    # @abstractmethod
+    # def __create_collection(self, colection_name:str):
+    #     ...
 
     @abstractmethod
-    def get_similar_words(self, word, top_n=10):
+    def get_similar_words(self, words:str, top_n=10):
         ...
 
-    @abstractmethod
-    def get_similar_vectors(self, vector, top_n=10):
-        ...
+    # @abstractmethod
+    # def get_similar_vectors(self, vector, top_n=10):
+    #     ...
--- epub_lib/gpt/epub_process/db/vector_store/vector_database.py
+++ epub_lib/gpt/epub_process/db/vector_store/vector_database.py
@@ -1,6 +1,6 @@
-from .interface import IVectorStore
 import chromadb
 from chromadb.config import Settings
+from interface import IVectorStore
 
 
 class VectorStore(IVectorStore):
@@ -8,14 +8,40 @@ class VectorStore(IVectorStore):
     collection:object
 
     def __init__(self, colection_name:str):
-        self.client = chromadb.Client(Settings(chroma_db_impl="duckdb+parquet"))
+        self.client = chromadb.Client()
         self.collection = self.__create_collection(colection_name)
         
     def __create_collection(self, colection_name:str):
         return self.client.get_or_create_collection(colection_name)
 
-    def add(self, document:list, metadata:dict, ids:list):
-        if len(document) == len(metadata.keys()) == len(ids):
-            self.collection.add(document, metadata, ids)
+    def add(self, document:list, ids:list, metadata:list = None):
+        if len(document) == len(ids):
+            self.collection.add(
+            documents=document, # we handle tokenization, embedding, and indexing automatically. You can skip that and add your own embeddings as well
+            ids=ids, # unique for each doc
+            )
+        
+
+    def get_similar_words(self, words:str, top_n=10):
+        results = self.collection.query(query_texts=words, n_results=top_n,
+        # where={"metadata_field": "is_equal_to_this"}, # optional filter
+        # where_document={"$contains":"search_string"}  # optional filter
+        )
+        return results
     
+    def get_synonym(self, word:str):
+        threshold = 1.8
+        result = vector.get_similar_words(word, top_n=1)
+        distance = result['distances'][0][0]
+        # return result['documents'][0][0] if distance > threshold else 'pepe'
+        return result
+
+
     
+
+#Chroma Vector Store
+vector = VectorStore("test")
+doc = '''Los seudocelomados1​, (Pseudocoelomata), asquelmintos (Aschelminthes), nematelmintos (Nemathelminthes) o blastocelomados (Blastoceolomata, nombre propuesto por Brusca & Brusca)2​ son una agrupación de filos cuya cavidad general no es de origen mesodérmico y recibe el nombre seudoceloma (o seudocele) o blastoceloma. Antiguamente formaron un filo único, los asquelmintos (del griego askos, ampolla o saco y helmins gusanos, gusanos que tienen un tubo, el digestivo, dentro de otro, la pared corporal), pero las diversas clases que lo componían son hoy consideradas como filos independientes.'''
+vector.add(doc.split(' '), 
+            [str(i) for i in range(1, len(doc.split(' '))+1)])
+print(vector.get_synonym('identificador'))
