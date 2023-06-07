8a2a66233d7359def1ef253895b3cc904fa48dcc
cesarllb <cesar98.linares@gmail.com>
2023-06-07 17:21:21 -0400

    fix entrypoint to only when upload_epub

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 from gpt.repo import GptRepo
 from upload_epub import process_upload_epub
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from pydantic import BaseModel
-import json
+
 
 app = FastAPI(description='An EPUB document processor designed to enhance your reading experience. With features such as consultation, summarization, explanation, definition, and translation of the book’s text, this tool makes it easy to understand and engage with your favorite books.')
 gpt_repo:GptRepo = None
@@ -11,41 +11,46 @@ class ReturnBase(BaseModel):
     operation: str
     result: str
 
-@app.post("/upload_epub/")
-def upload_epub(file: UploadFile = File(...)) -> str:
+def check_gpt_repo():
+    if gpt_repo is None:
+        raise HTTPException(status_code=400, detail="upload_epub must be called first")
+
+
+@app.post("/upload_epub/", description="Upload an EPUB file for processing.")
+async def upload_epub(file: UploadFile = File(...)) -> str:
     global gpt_repo
     gpt_repo =  process_upload_epub(file)
     return_data = ReturnBase(operation = "epub_gpt.upload_epub", result = "success")
     return return_data.json()
 
-@app.post("/resume/")
-def resume(topic:str) -> str:
+@app.post("/resume/", description="Generate a summary of the uploaded EPUB file on a given topic.")
+async def resume(topic:str, check: None = Depends(check_gpt_repo)) -> str:
     data = gpt_repo.resume(topic)
     return_data = ReturnBase(operation = "epub_gpt.resume", result = data)
     return return_data.json()
 
-@app.post("/explain_text/")
-def explain_text(text:str) -> str:
+@app.post("/explain_text/", description="Provide an explanation of a given text from the uploaded EPUB file.")
+async def explain_text(text:str, check: None = Depends(check_gpt_repo)) -> str:
     data =  gpt_repo.explain_text(text)
     return_data = ReturnBase(operation = "epub_gpt.explain_text", result = data)
     return return_data.json()
 
 
-@app.post("/describe_own_name/")
-def describe_own_name(noun:str) -> str:
+@app.post("/describe_own_name/", description="Provide a description of a given noun from the uploaded EPUB file.")
+async def describe_own_name(noun:str, check: None = Depends(check_gpt_repo)) -> str:
     data = gpt_repo.describe_own_name(noun)
     print(type(data))
     return_data = ReturnBase(operation = "epub_gpt.describe_own_name", result = data)
     return return_data.json()
     
-@app.post("/translate_text/")
-def translate_text(text:str, language:str) -> str:
+@app.post("/translate_text/", description= "Translate a given text from the uploaded EPUB file into a specified language.")
+async def translate_text(text:str, language:str, check: None = Depends(check_gpt_repo)) -> str:
     data = gpt_repo.translate_text(text, language)
     return_data = ReturnBase(operation = "epub_gpt.translate_text", result = data)
     return return_data.json()
 
-@app.post("/define_word/")
-def define_word(word:str) -> str:
+@app.post("/define_word/", description="Provide a definition of a given word from the uploaded EPUB file")
+async def define_word(word:str, check: None = Depends(check_gpt_repo)) -> str:
     data = gpt_repo.define_word(word)
     return_data = ReturnBase(operation = "epub_gpt.define_word", result = data)
     return return_data.json()
@@ -54,9 +59,9 @@ if __name__ =='__main__':
     import uvicorn
     try:
         uvicorn.run(
-                app(), 
+                app, 
                 host= '127.0.0.1', 
-                port= '8081',
+                port= 8081,
                 )
     except Exception as e:
         print(e)
